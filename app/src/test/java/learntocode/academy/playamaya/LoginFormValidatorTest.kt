006feffbf9a1c9b83003d3670d6b751acc13721a
cupc4ke <miguel.martinez@ciencias.unam.mx>
2021-05-11 15:38:17 -0500

    implementación pruebas username y password invalidos

--- app/src/test/java/learntocode/academy/playamaya/LoginFormValidatorTest.kt
+++ app/src/test/java/learntocode/academy/playamaya/LoginFormValidatorTest.kt
@@ -7,14 +7,79 @@ import org.junit.runners.JUnit4
 import com.google.common.truth.Truth.assertThat
 
 @RunWith(JUnit4::class)
-class loginFormValidatorTest{
+class LoginFormValidatorTest{
+
+    @Test
+    fun `Username valido isValidUsername`() {
+        val username = "thelegend27"
+        val validatedResult = LoginFormValidator.isValidUsername(username)
+        assertThat(validatedResult).isEqualTo(true)
+    }
+
+    @Test
+    fun `Password valido isValidPassword`() {
+        val password = "bOrati5a!Mazin*"
+        val validatedResult = LoginFormValidator.isValidPwd(password)
+        assertThat(validatedResult).isEqualTo(true)
+    }
 
     @Test
     fun `Username y password validos`() {
         val username = "thelegend27"
         val password = "bOrati5a!Mazin*"
-        val validatedResult = loginFormValidator.validateInput(username, password)
+        val validatedResult = LoginFormValidator.validateInput(username, password)
         assertThat(validatedResult).isEqualTo(true)
     }
 
+
+    @Test
+    fun `Username invalido isValidUsername`() {
+        val username = "lege"
+        val validatedResult = LoginFormValidator.isValidUsername(username)
+        assertThat(validatedResult).isEqualTo(false)
+    }
+
+
+    @Test
+    fun `Password invalido, no tiene mayusculas isValidPassword`() {
+        val password = "borati5a!mazin*"
+        val validatedResult = LoginFormValidator.isValidPwd(password)
+        assertThat(validatedResult).isEqualTo(false)
+    }
+
+    @Test
+    fun `Password invalido, no tiene minusculas isValidPassword`() {
+        val password = "BORATI5A!MAZIN*"
+        val validatedResult = LoginFormValidator.isValidPwd(password)
+        assertThat(validatedResult).isEqualTo(false)
+    }
+
+    @Test
+    fun `Password invalido, no tiene caracteres especiales isValidPassword`() {
+        val password = "bOrati5aMazin"
+        val validatedResult = LoginFormValidator.isValidPwd(password)
+        assertThat(validatedResult).isEqualTo(false)
+    }
+
+    @Test
+    fun `Password invaldio, no tiene numeros isValidPassword`() {
+        val password = "bOratia!Mazin*"
+        val validatedResult = LoginFormValidator.isValidPwd(password)
+        assertThat(validatedResult).isEqualTo(false)
+    }
+
+    @Test
+    fun `Password invaldio, tiene menos de 8 caracteres isValidPassword`() {
+        val password = "bO!M4n*"
+        val validatedResult = LoginFormValidator.isValidPwd(password)
+        assertThat(validatedResult).isEqualTo(false)
+    }
+
+    @Test
+    fun `Username y password invalidos`() {
+        val username = "lege"
+        val password = "password"
+        val validatedResult = LoginFormValidator.validateInput(username, password)
+        assertThat(validatedResult).isEqualTo(false)
+    }
 }
