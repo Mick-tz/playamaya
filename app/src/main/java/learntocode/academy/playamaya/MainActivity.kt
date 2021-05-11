27e74adc94fc0749321c1a49a4f51bd5ba162629
cupc4ke <miguel.martinez@ciencias.unam.mx>
2021-05-11 15:38:43 -0500

    validación basica login form en main activity

--- app/src/main/java/learntocode/academy/playamaya/MainActivity.kt
+++ app/src/main/java/learntocode/academy/playamaya/MainActivity.kt
@@ -2,25 +2,34 @@ package learntocode.academy.playamaya
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private var usernameEditText: EditText? = null
-    private var passwordEditText: EditText? = null
-    private var loginButton: Button? = null
-    private var registerButton: Button? = null
     private var helpTextView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        usernameEditText = findViewById<EditText>(R.id.usernameEditText)
-        passwordEditText = findViewById<EditText>(R.id.passwordEditText)
-        loginButton = findViewById<Button>(R.id.loginButton)
-        registerButton = findViewById<Button>(R.id.registerButton)
+        val usernameEditText: EditText = findViewById<EditText>(R.id.usernameEditText)
+        val passwordEditText: EditText = findViewById<EditText>(R.id.passwordEditText)
+        val loginButton: Button = findViewById<Button>(R.id.loginButton)
+        val registerButton: Button = findViewById<Button>(R.id.registerButton)
         helpTextView = findViewById<TextView>(R.id.helpTextView)
+
+        helpTextView?.text = ""
+
+        loginButton.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                if (LoginFormValidator.validateInput(usernameEditText as Any, passwordEditText as Any)) {
+                    helpTextView?.text = "Validando password y username!"
+                } else {
+                    helpTextView?.text = "Nombre de usuario o password incorrectos"
+                }
+            }
+        })
     }
 }
